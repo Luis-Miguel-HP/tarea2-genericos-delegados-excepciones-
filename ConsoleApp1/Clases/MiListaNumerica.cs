59c214eed45e976285d4dffd48ade3a2436046e1
LuisMhp <119384750+Luis-Miguel-HP@users.noreply.github.com>
2026-02-04 14:46:55 -0400

    Agregando clases

---
+++ ConsoleApp1/Clases/MiListaNumerica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Clases
+{
+    public class MiListaNumerica<T> where T : INumber<T>
+
+    {
+        private List<T> Lista;
+
+
+        public MiListaNumerica() { 
+        
+            Lista = new List<T>();
+        
+        }
+
+        public void agregarNumeroLista()
+        {
+
+        }
+    }
+
+
+    
+  
+}
